e0b20526a22f05fcc79828fa0bbabeb0e4e05002
zhangjianjun <18720438806@163.com>
2017-03-16 11:46:13 +0800

    test

--- js/js.js
+++ js/js.js
@@ -1,8 +1,8 @@
 $(function() {
     $('nav a').mouseover(function() {
-        if ($(this).is('.back')) {
-            return;
-        }
-        $('.back').stop().animate({ left: ($(this).index() - 1) * ($(this).width() + parseInt($(this).css("margin-left"), 10) + 4) }, 500, 'easeOutElastic')
-    })
+            if ($(this).is('.back')) {
+                return;
+            }
+            $('.back').stop().animate({ left: ($(this).index() - 1) * ($(this).width() + parseInt($(this).css("margin-left"), 10) + 4) }, 500, 'easeOutElastic')
+        }) //test js
 })
