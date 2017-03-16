ecaa32cd14c19cd3df3f700e042736c49ef9afa0
zhangjianjun <18720438806@163.com>
2017-03-16 11:48:47 +0800

    test visual studio code

--- js/js.js
+++ js/js.js
@@ -4,5 +4,5 @@ $(function() {
                 return;
             }
             $('.back').stop().animate({ left: ($(this).index() - 1) * ($(this).width() + parseInt($(this).css("margin-left"), 10) + 4) }, 500, 'easeOutElastic')
-        }) //test js
+        }) //test visual studio code
 })
